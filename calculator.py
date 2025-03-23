a5d5f1aea4d98bbe72dc4afcafe43a8bbb615592
Nginacloud <nginadorcas451@gmail.com>
2025-03-23 09:58:45 +0300

    DataStructures

--- calculator.py
+++ calculator.py
@@ -13,19 +13,3 @@ def math_operation(num1, num2, operation):
     else:
         return "invalid operation"
     
-def main():
-    try:
-        num1 = float(input("Enter number: "))
-        num2 = float(input("Enter number: "))
-        operation = input("Enter operation sign (+, -, *, /): ")
-
-        result = math_operation(num1, num2, operation)
-        if ("Error") in str(result):
-            print(result)
-        else:
-            print(f"{num1} {operation} {num2} = {result}")
-    except ValueError:
-            print("Invalid input. Please enter numeric values.")
-
-if __name__ == "__main__":
-    main()
